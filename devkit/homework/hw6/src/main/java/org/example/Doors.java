0db322f834f48f83f35eaed65d94528cff0d3330
sv1t0898 <sv1t0898@gmail.com>
2024-11-11 21:31:05 +0300

    add devkit/hw6

--- devkit/homework/hw6/src/main/java/org/example/Doors.java
+++ devkit/homework/hw6/src/main/java/org/example/Doors.java
@@ -1,11 +1,18 @@
 package org.example;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Arrays;
 import java.util.Random;
+@Setter
+@Getter
 
 public class Doors {
 
+    @Setter
     private int selectedDoor;
+    @Setter
     private int openedDoor = -1;
     private final int winningDoor;
     private final Boolean[] doors = new Boolean[3];
@@ -18,30 +25,6 @@ public class Doors {
                     doors[i] = false;
                 }
         }
-    }
-
-    public Boolean[] getDoors(){
-        return doors;
-    }
-
-    public int getWinningDoor() {
-        return winningDoor;
-    }
-
-    public int getOpenedDoor(){
-        return openedDoor;
-    }
-
-    public void setOpenedDoor(int openedDoor) {
-        this.openedDoor = openedDoor;
-    }
-
-    public int getSelectedDoor() {
-        return selectedDoor;
-    }
-
-    public void setSelectedDoor(int selectedDoor) {
-        this.selectedDoor = selectedDoor;
     }
 
     @Override
