9107ee88466466285a2b1fa397da1287450a3b6e
dude-h <dude@dude-h.de>
2024-11-26 21:52:29 +0300

    add junjava/hw1

--- 5.junjava/homework/hw1/src/main/java/org/example/Main.java
+++ 5.junjava/homework/hw1/src/main/java/org/example/Main.java
@@ -1,9 +1,25 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+//        Напишите программу, которая использует Stream API для обработки списка чисел.
+//                Программа должна вывести на экран среднее значение всех четных чисел в списке
+
+
+        List<Integer> listNumber = new ArrayList();
+        for (int i = 1; i < 11; i++) {
+            listNumber.add(i);//new Random().nextInt(1000));
+        }
+
+        System.out.println(listNumber.stream().filter(n -> n % 2 == 0)
+                .collect(Collectors.averagingInt(i -> i)));
+
 
     }
 }
