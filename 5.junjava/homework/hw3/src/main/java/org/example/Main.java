cc5bcee5c0887168badad2dc47e7f69dcfcc0b2a
sv1t0898 <sv1t0898@gmail.com>
2024-12-03 20:22:36 +0300

    add junjava/hw3

--- 5.junjava/homework/hw3/src/main/java/org/example/Main.java
+++ 5.junjava/homework/hw3/src/main/java/org/example/Main.java
@@ -7,8 +7,94 @@ package org.example;
 // Реализуйте методы для добавления, обновления и удаления объектов Person.
 
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        Person person = new Person("new name 1", 1, "new surname");
+        Person person1;
+        Person person2;
+        Person person3;
+
+        serializ("ser.bin", person);
+        serializ("ser.xml", person);
+        serializ("ser.json", person);
+
+        person1 = deSerializ("ser.bin");
+        person2 = deSerializ("ser.xml");
+        person3 = deSerializ("ser.json");
+
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
+
 
     }
+
+//    public static void binSerializ(String path, Object object){
+//        try(FileOutputStream fileOutputStream = new FileOutputStream(path)) {
+//            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+//            objectOutputStream.writeObject(object);
+//        }catch (IOException e){
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    public static Object binDeSerializ(String path){
+//        try(FileInputStream fileInputStream = new FileInputStream(path)) {
+//            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+//            return objectInputStream.readObject();
+//        } catch (IOException | ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
+    public static void serializ(String path, Object object){
+        try{
+            if(path.endsWith(".json")){
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+                objectMapper.writeValue(new File(path), object);
+            } else if (path.endsWith(".xml")) {
+                XmlMapper xmlMapper = new XmlMapper();
+                xmlMapper.writeValue(new File(path), object);
+            } else if (path.endsWith(".bin")) {
+                FileOutputStream fileOutputStream = new FileOutputStream(path);
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+                objectOutputStream.writeObject(object);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static Person deSerializ(String path){
+        Person person = null;
+        try{
+            if(path.endsWith(".json")){
+                ObjectMapper objectMapper = new ObjectMapper();
+                person = objectMapper.readValue(new File(path), Person.class);
+            } else if (path.endsWith(".xml")) {
+                XmlMapper xmlMapper = new XmlMapper();
+                person = xmlMapper.readValue(new File(path), Person.class);
+            } else if (path.endsWith(".bin")) {
+                FileInputStream fileInputStream = new FileInputStream(path);
+                ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+                person = (Person) objectInputStream.readObject();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return person;
+    }
+
+
 }
--- 5.junjava/homework/hw3/src/main/java/org/example/Person.java
+++ 5.junjava/homework/hw3/src/main/java/org/example/Person.java
@@ -4,5 +4,19 @@ import java.io.Serializable;
 
 public class Person implements Serializable {
     public String name;
+    transient String surname;
     public int age;
+
+    public Person (){}
+
+    public Person(String name, int age, String surname){
+        this.age = age;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Name - %s; Age - %s, Surname - %s", name, age, surname);
+    }
 }
